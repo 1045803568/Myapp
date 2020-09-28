8aa265543b60c2bc16c8e5f57c7c6018d7064974
1045803568 <1045803568@qq.com>
2020-09-28 17:27:17 +0800

    更新菜单栏

--- app/src/main/java/com/example/myapplication/RateExchange.java
+++ app/src/main/java/com/example/myapplication/RateExchange.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -64,5 +66,21 @@ public class RateExchange extends AppCompatActivity {
         Log.i(TAG,"openOne:wonRate"+wonRate);
 
         startActivity(config);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1&&resultCode==2){
+            Bundle bd = data.getExtras();
+            dallarRate = bd.getFloat("dollar_rate2",0.0000f);
+            euroRate = bd.getFloat("euro_rate2",0.0000f);
+            wonRate = bd.getFloat("won_rate2",0.0000f);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public boolean openContextMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
